a8f83e95bc99acbf969d7a8d5547403c373a39c2
Zero2164 <61925456+Zero2164@users.noreply.github.com>
2021-04-12 22:55:09 +1000

    add styling to projects page /  update to links

--- HELP/client/src/app/projects/projects.component.ts
+++ HELP/client/src/app/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngular, faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faChessKnight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-projects',
@@ -9,8 +11,10 @@ export class ProjectsComponent implements OnInit {
   itemsPerSlide = 2;
   singleSlideOffset = false;
   noWrap = false;
-
-
+  fahtml5 = faHtml5;
+  faChessKnight = faChessKnight;
+  faAngular = faAngular;
+  
   constructor() { }
 
   ngOnInit(): void {
