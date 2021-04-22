98dd36cfc08fdbd12f0c864472fa1e4682d3405c
Zero2164 <61925456+Zero2164@users.noreply.github.com>
2021-04-22 16:29:22 +1000

    update to home video, created autoloop in chrome

--- HELP/client/src/app/home/home.component.ts
+++ HELP/client/src/app/home/home.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faArrowRight, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faCode, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
   faArrowRight = faArrowRight;
-  faLinkedin = faLinkedin ;
+  faLinkedin = faLinkedin;
   faGithub = faGithub;
   faEnvelope = faEnvelope;
-
+  faCode = faCode;
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
   }
 
 
+ 
 }
